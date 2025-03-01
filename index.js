f9d4429397da5a2c3d8678ebb77fff1e1d5b05ae
CaiqueRNeves <caiquerabelo2015@hotmail.com>
2025-03-01 17:08:46 -0300

    main

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const express = require('express');
 const calculadora = express();
 const bodyparser = require('body-parser');
 
+// Configurar o middleware apenas uma vez
 calculadora.use(bodyparser.urlencoded({extended: true}));
 
+// Rota para a página inicial (soma)
 calculadora.get('/', (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+// Rota para processamento da soma
 calculadora.post('/', (req, res) => {
   const n1 = Number(req.body.num1);
   const n2 = Number(req.body.num2);
@@ -17,10 +20,12 @@ calculadora.post('/', (req, res) => {
   res.send("O resultado da operação é: " + soma);
 });
 
+// Rota para a página de subtração
 calculadora.get('/subtracao', (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+// Rota para processamento da subtração
 calculadora.post('/subtracao', (req, res) => {
   const n1 = Number(req.body.num1);
   const n2 = Number(req.body.num2);
@@ -29,6 +34,7 @@ calculadora.post('/subtracao', (req, res) => {
   res.send("O resultado da operação é: " + subtracao);
 });
 
+// Iniciar o servidor
 calculadora.listen(8080, (erro) => {
   if(erro) {
     console.log("Erro ao iniciar servidor:", erro);
