f777748898bdab5db12ac8c022806bc7f3318509
CaiqueRNeves <caiquerabelo2015@hotmail.com>
2025-03-01 17:33:14 -0300

    main

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-// run `node index.js` in the terminal
 const express = require('express');
 const calculadora = express();
 const bodyparser = require('body-parser');
 
-// Configurar o middleware apenas uma vez
 calculadora.use(bodyparser.urlencoded({extended: true}));
 
-// Rota para a página inicial (soma)
+
 calculadora.get('/', (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-// Rota para processamento da soma
+
 calculadora.post('/', (req, res) => {
   const n1 = Number(req.body.num1);
   const n2 = Number(req.body.num2);
@@ -20,12 +18,12 @@ calculadora.post('/', (req, res) => {
   res.send("O resultado da operação é: " + soma);
 });
 
-// Rota para a página de subtração
+
 calculadora.get('/subtracao', (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-// Rota para processamento da subtração
+
 calculadora.post('/subtracao', (req, res) => {
   const n1 = Number(req.body.num1);
   const n2 = Number(req.body.num2);
@@ -46,7 +44,19 @@ calculadora.post('/Multiplicacao', (req, res) => {
   res.send("O resultado da operação é: " + multiplicacao);
 });
 
-// Iniciar o servidor
+calculadora.get('/Divisao', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+calculadora.post('/Divisao', (req, res) => {
+  const n1 = Number(req.body.num1);
+  const n2 = Number(req.body.num2);
+  
+  const divisao = n1 * n2;
+  res.send("O resultado da operação é: " + divisao);
+});
+
+
 calculadora.listen(8080, (erro) => {
   if(erro) {
     console.log("Erro ao iniciar servidor:", erro);
