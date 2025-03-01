8505bed519b0163c114cd6e41b2a2e3a167424f0
CaiqueRNeves <caiquerabelo2015@hotmail.com>
2025-03-01 16:19:09 -0300

    main

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ calculadora.post('/' , (req,res)=>{
   const n1 = Number(req.body.num1)
   const n2 = Number(req.body.num2)
 
-  const soma = n1+ n2
-  res.send("O resultado da operação é: "+ soma)
+  const soma = n1 + n2
+  res.send(<h1>"O resultado da operação é: "+ soma +</h1>)
 })
 
 
